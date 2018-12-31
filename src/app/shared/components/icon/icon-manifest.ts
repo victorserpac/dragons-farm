f2d6ed299198fec91ad8573ea9bb8db2c4cfd146
Victor Serpa do Carmo <victorserpac@gmail.com>
2018-12-31 18:48:40 -0200

    Change icons for SVG

--- src/app/shared/components/icon/icon-manifest.ts
+++ src/app/shared/components/icon/icon-manifest.ts
@@ -3,3 +3,5 @@ import '!svg-sprite-loader!svgo-loader!../../../../assets/icons/config.svg';
 import '!svg-sprite-loader!svgo-loader!../../../../assets/icons/success.svg';
 import '!svg-sprite-loader!svgo-loader!../../../../assets/icons/trash.svg';
 import '!svg-sprite-loader!svgo-loader!../../../../assets/icons/warning.svg';
+import '!svg-sprite-loader!svgo-loader!../../../../assets/icons/create.svg';
+import '!svg-sprite-loader!svgo-loader!../../../../assets/icons/logout.svg';
